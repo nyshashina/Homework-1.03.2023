df426eeeab11341cf205c56ea09cac2484873470
nyshashina <nyshashina@gmail.com>
2023-02-28 23:03:10 +0300

    corrected

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,14 +3,31 @@ void ThreeSymEls(string newstring)
 {
     char[] separators = new char[] {'"'};
     string[] subs = newstring.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-    string[] otherstring = new string[subs.Length];
+
+    int count = 0;
     for(int i = 0; i < subs.Length; i++)
     {
         if(subs[i].Length <= 3 && subs[i] != " ")
         {
-            otherstring[i] = subs[i];
+            count++;
         }
-        Console.Write($"{otherstring[i]} ");
+    }
+
+    string[] otherstring = new string[count];
+    count = 0;
+
+    for(int i = 0; i < subs.Length; i++)
+    {
+        if(subs[i].Length <= 3 && subs[i] != " ")
+        {
+            otherstring[count] = subs[i];
+            count++;
+        }
+    }
+    
+    foreach(string el in otherstring)
+    {
+        Console.Write($"{el} ");
     }
     Console.WriteLine();
 }
