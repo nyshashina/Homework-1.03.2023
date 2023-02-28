5643bbfe9f6e8fea0632460e86760b4864eaa079
nyshashina <nyshashina@gmail.com>
2023-02-28 21:52:18 +0300

    find string length

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Из имеющегося массива строк формировать массив строк, длина которых меньше, либо равна 3 символам
+
+Console.WriteLine("Введите текст");
+string coords1 = Console.ReadLine () ?? "0";
+int li = coords1.Length;
+Console.WriteLine($"{li}");
