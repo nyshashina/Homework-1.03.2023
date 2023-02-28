b628920afb249221c2c00a4d34b58ce8f68c5217
nyshashina <nyshashina@gmail.com>
2023-02-28 22:37:57 +0300

    find strings shorter than 3 symbols

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,6 +1,19 @@
 ﻿// Из имеющегося массива строк формировать массив строк, длина которых меньше, либо равна 3 символам
+void ThreeSymEls(string newstring)
+{
+    char[] separators = new char[] {'"'};
+    string[] subs = newstring.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    string[] otherstring = new string[subs.Length];
+    for(int i = 0; i < subs.Length; i++)
+    {
+        if(subs[i].Length <= 3 && subs[i] != " ")
+        {
+            otherstring[i] = subs[i];
+        }
+        Console.Write($"{otherstring[i] }");
+    }
+}
 
-Console.WriteLine("Введите текст");
-string coords1 = Console.ReadLine () ?? "0";
-int li = coords1.Length;
-Console.WriteLine($"{li}");
+Console.WriteLine("Введите строки массива, разделив их кавычками");
+string str1 = Console.ReadLine () ?? "0";
+ThreeSymEls(str1);
